d916a793a07b5639a3f032e8331a613ca3465911
isabelletrask <traski@sheridancollege.ca>
2022-02-02 09:00:11 -0500

    added constructor for object

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -28,6 +28,10 @@ public class Card
         value = givenValue;
     }
 
+    Card() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     /**
      * @return the suit
      */
